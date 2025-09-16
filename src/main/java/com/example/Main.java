538a27361c9820a266c7a80efb812d14b251dfa7
Paloma Gomez Abad <paloma.gomez@gruposantander.com>
2025-09-16 10:18:39 +0200

    Commit for Bacis arithmetic operation methods

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,13 +2,7 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        // Demonstrate the functionality of the calculator
-        int a = 10;
-        int b = 5;  
-        System.out.println("Addition: " + add(a, b));
-        System.out.println("Subtraction: " + subtract(a, b));
-        System.out.println("Multiplication: " + multiply(a, b));
-        System.out.println("Division: " + divide(a, b));
+      //to do
     }
    //methods for basic arithmetic operations
     public static int add(int a, int b) {
